1fbc79fc5c085dce52963fa4f604cb7e6e94761b
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-11 15:03:19 -0500

    talked about hoisting scopes, var vs let and const

--- HelloFunctionsScopes.js
+++ HelloFunctionsScopes.js
@@ -47,8 +47,55 @@ f2(3000, f1)
 
 console.log("=================================Global Scope")
 
+//anything that is globally scoped can be accessed anywhere in our script (the JS file)
+console.log(a)
+
+//vars are globally scoped (they're HOISTED) - 
+//all named functions and vars are hoisted and globally scoped
+var a = 5
+
+console.log(a) //a, of course, is visible and defined after its declaration
+
+//Let's attempt the same thing with a let
+
+//console.log(b) <- This is ILLEGAL! lets and consts are NOT HOISTED
+
+let b = 5
+
+//this is actually a good thing imo - we usually don't want to have access to undefined variables
 
 
 console.log("=================================Local Scope (block scope vs function scope)")
 
+//block scope----/
 
+//any variable inside a non-function block is block scoped
+if(true){
+    var c = "I'm a var in a block"
+    let d = "I'm a let in a block"
+}
+
+console.log(c) //vars are globally scoped no matter what. so they're visible outside of a block
+//console.log(d) //lets and consts are NOT visible outside of any block they're defined in
+
+//function scope----/
+
+//any variable defined in a function is function scoped
+function scopeTest(){
+    var e = "I'm a var in a function - function scoped"
+}
+
+console.log(e)
+
+/* The main difference between block and function scope
+
+-vars are not visible outside of a function they're defined in
+-BUT... vars ARE visible outside of any non-function block they're defined in
+
+-lets and consts are only visible within the blocks they're defined in, period
+
+Since we only really use let and const these days...
+we don't have to worry as much about weird var behavior
+
+also, functions behave similarly to vars in that they globally scoped and hoisted
+but I actually like that functions are globally scoped and hoisted */
