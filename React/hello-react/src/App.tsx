b6fac9f9d29af3cb2033e20cde44e12dfa08a293
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-12 14:22:54 -0500

    started initial React app, just scratching the surface

---
+++ React/hello-react/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h3>Hello React! We'll make great modern UIs with you :)</h3>
+
+      <p>We are writing TSX, which is mostly like HTML, but it's for React. There are small differences</p>
+
+      {/* This is a comment in TSX */}
+
+      <h4>React will reload the app with every saved change to the source code. </h4>
+
+      {/* Rendering some custom components below */}
+
+      
+
+    </>
+  )
+}
+
+export default App
