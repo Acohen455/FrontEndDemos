ed065f1f6673a73064ebea5c95c0c6999ecf2ea1
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-12 11:06:37 -0500

    better fetch handling

--- HelloFetch/HelloFetch.js
+++ HelloFetch/HelloFetch.js
@@ -1,2 +1,44 @@
 //PokeAPI base URL - making a variable that holds the base URL to reach pokeAPI. Good for cleaner/standardized code
 const url = "https://pokeapi.co/api/v2/pokemon/"
+
+//DOM selection for the elements we'll fill with data after the fetch
+const pokename = document.getElementById("pokename")
+const poketype = document.getElementById("poketype")
+const pokenum = document.getElementById("pokenum")
+const pokepic = document.getElementById("pokepic")
+
+//when the user clicks the button, take their input and use it in the fetchData function
+document.getElementById("btn").onclick = fetchData
+
+
+/* This function will return some Pokemon data from PokeAPI 
+   It will use a fetch request to return a promise object
+   The promise object will get filled with pokemon data OR it will fail */
+async function fetchData(){
+
+    //First, we need to gather the user input from the input box
+    const userInput = document.getElementById("userInput").value
+
+    console.log(userInput) //Just to see the value
+
+    //Now, we can use the fetch() function to send a GET request to pokeAPI
+    //(Fetch sends a GET default by the way)
+    try{
+        const response = await fetch(url + userInput)
+
+        if(!response.ok) {
+            throw new Error("It got away!")
+        }
+
+        //turn the response data into a JS object 
+        const data = await response.json()
+
+        console.log(data) //just to see the data
+    } catch {
+        //TODO: error handling code
+    } finally {
+        //TODO: cleanup code
+    }
+
+
+}
