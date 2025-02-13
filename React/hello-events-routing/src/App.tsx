c2645c44302f60139c93fd7a7cce4399f8b38e4d
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-13 13:47:00 -0500

    routed calc component

--- React/hello-events-routing/src/App.tsx
+++ React/hello-events-routing/src/App.tsx
@@ -1,10 +1,25 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Calculation } from './Components/CalcuationComponent/Calculation'
 
 function App() {
 
   return (
     <>
+      <h4>Welcome to the Events/Routing Demo</h4>
+      <h5>Navigate to different URL endpoints to see different components</h5>
+
+      {/* Defining Routes 
       
+      TODO: anatomy notes
+      */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="calc" element={<Calculation/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
