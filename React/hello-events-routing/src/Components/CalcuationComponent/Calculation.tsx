319ea645d83579934c3235122019df7618a42240
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-13 14:14:14 -0500

    state vars

--- React/hello-events-routing/src/Components/CalcuationComponent/Calculation.tsx
+++ React/hello-events-routing/src/Components/CalcuationComponent/Calculation.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react"
 import "./Calculation.css"
 
 export const Calculation:React.FC = () => {
 
-
+    //Some state variables to store the user's input and the result of the calculation
+    const [SideA, setSideA] = useState<number>(0)
+    const [SideB, setSideB] = useState<number>(0)
+    const [result, setResult] = useState<number>(0)
 
     return(
         <div className="input-container">
