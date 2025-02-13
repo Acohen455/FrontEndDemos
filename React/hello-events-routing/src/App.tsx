3f7715b93dd0a3e3d006eb4ae761558159c78a33
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-02-13 13:51:46 -0500

    two different components, two different routes

--- React/hello-events-routing/src/App.tsx
+++ React/hello-events-routing/src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Calculation } from './Components/CalcuationComponent/Calculation'
+import { CharacterList } from './Components/CharacterListComponent/CharacterList'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <h4>Welcome to the Events/Routing Demo</h4>
       <h5>Navigate to different URL endpoints to see different components</h5>
+      <h5>In a real app, we wouldn't put this statically in app.tsx^ everything would be routed probably</h5>
 
       {/* Defining Routes 
       
@@ -17,6 +19,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="calc" element={<Calculation/>}/>
+          <Route path="char" element={<CharacterList/>}/>
         </Routes>
       </BrowserRouter>
 
